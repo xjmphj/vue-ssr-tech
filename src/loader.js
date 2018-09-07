80d87547497c1973d95ae3a7bdf5930950647508
xiejunmei <894386308@qq.com>
2018-09-07 18:53:00 +0800

    测试添加loader

---
+++ src/loader.js
@@ -0,0 +1,22 @@
+import { getOptions } from 'loader-utils';
+import validateOptions from 'schema-utils';
+
+const schema = {
+  type: 'object',
+  properties: {
+    test: {
+      type: 'string'
+    }
+  }
+};
+
+export default function(source) {
+  const options = getOptions(this);
+
+  validateOptions(schema, options, 'Example Loader');
+
+  // 对资源应用一些转换……
+  source = source.replace(/\[name\]/g, options.name);
+
+  return `export default ${ JSON.stringify(source) }`;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,6 @@ const config = {
           test:/\.jsx$/,
           loader:'babel-loader'
         },
-        
         {
           test:/\.(gif|jpg|png|svg|jpeg)$/,
           use:
@@ -77,10 +76,22 @@ if(isDev){
       'stylus-loader'
     ]
   });
-  //方便调试，用于
+  //方便调试，cheap-module-source-map 代表不带列映射的 SourceMap，将加载的 Source Map 简化为每行单独映射。
   // config.devtool = '#cheap-module-source-map';
+  // source-map	在一个单独的文件中产生一个完整且功能完全的文件。这个文件具有最好的source map，但是它会减慢打包速度；
+  // cheap-module-source-map	在一个单独的文件中生成一个不带列映射的map，不带列映射提高了打包速度，但是也使得浏览器开发者工具只能对应到具体的行，不能对应到具体的列（符号），会对调试造成不便；
+  // eval-source-map	使用eval打包源文件模块，在同一个文件中生成干净的完整的source map。这个选项可以在不影响构建速度的前提下生成完整的sourcemap，但是对打包后输出的JS文件的执行具有性能和安全的隐患。在开发阶段这是一个非常好的选项，在生产阶段则一定不要启用这个选项；
+  // cheap-module-eval-source-map	这是在打包文件时最快的生成source map的方法，生成的Source Map 会和打包后的JavaScript文件同行显示，没有列映射，和eval-source-map选项具有相似的缺点；
+  
   config.devtool = '#cheap-module-eval-source-map';
   //配置一个本地开发环境的服务
+
+  // devServer 配置项  
+  // contentBase	默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到“dist"目录）
+  // port	设置默认监听端口，如果省略，默认为”8080“
+  // inline	设置为true，当源文件改变时会自动刷新页面
+  // historyApiFallback	在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+  
   config.devServer = {
     port:8000,
     host:'0.0.0.0',
